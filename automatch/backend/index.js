2579e129bf48a9105bc47db0ad9c4fd452260d76
imad-elkhiati <elkhiati0imad@gmail.com>
2025-03-07 09:21:44 +0000

    add backend folder

--- automatch/backend/index.js
+++ automatch/backend/index.js
@@ -6,16 +6,12 @@ import dotenv from 'dotenv';
 dotenv.config();  
 
 const app = express();
-const PORT = process.env.PORT || 6000;
-
-
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());  
 app.use(express.json());  
 
-
 mongoose
-  
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
