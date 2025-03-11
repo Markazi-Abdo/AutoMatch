ecbda902afeeaad4ee79728df3fbd346e28d68b7
Abdellatif Markazi <abmxrkxzi2005@gmail.com>
2025-03-11 17:20:57 +0000

    Fix insert

--- AutoMatch/backend/routes/car.routes.js
+++ AutoMatch/backend/routes/car.routes.js
@@ -2,8 +2,12 @@ import express from "express"
 import Car from "../index.js"
 const autoRouter = express.Router();
 
-router.post('/', async (req, res) => {
+autoRouter.post('/', async (req, res) => {
     try {
+      const findMarque = await Car.findOne({ "marque.nom": req.body.marque.nom });
+      if(findMarque){
+        findMarque.models.push(req.body.models)
+      }
       const car = new Car(req.body);
       await car.save();
       res.status(201).json(car);
@@ -12,25 +16,25 @@ router.post('/', async (req, res) => {
     }
   });
   
-  router.put('/:id', async (req, res) => {
-    try {
-      const car = await Car.findOneAndUpdate({ id: req.params.id }, req.body, { new: true });
-      if (!car) return res.status(404).json({ error: 'Car not found' });
-      res.json(car);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  });
-  
-  router.delete('/:id', async (req, res) => {
-    try {
-      const car = await Car.findOneAndDelete({ id: req.params.id });
-      if (!car) return res.status(404).json({ error: 'Car not found' });
-      res.json({ message: 'Car deleted' });
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  });
+autoRouter.put('/:id', async (req, res) => {
+  try {
+    const car = await Car.findOneAndUpdate({ id: req.params.id }, req.body, { new: true });
+    if (!car) return res.status(404).json({ error: 'Car not found' });
+    res.json(car);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+autoRouter.delete('/:id', async (req, res) => {
+  try {
+    const car = await Car.findOneAndDelete({ id: req.params.id });
+    if (!car) return res.status(404).json({ error: 'Car not found' });
+    res.json({ message: 'Car deleted' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 
 export default autoRouter;
